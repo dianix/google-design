3ab9d3e3aefe0339a6c559dc55e00a2dab1f602d
Diana Vázquez <dianavazquez.dcg@gmail.com>
2017-06-29 17:44:16 -0500

    funcion para header en cosntruccion

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var cargarPagina = function(){
+    navegadorCambio();
+}
+
+function navegadorCambio() {
+    var navbar = $("#navegador");
+    var buscar = $("#buscar");
+    var logoNav = $("#logo-nav");
+    var logoTop = $("#logo-top");
+    console.log(window.scrollY)
+    
+    if (window.scrollY > 400) {
+        buscar.removeClass("navbar__buscar--top");
+        logoTop.css("display", "none");
+        logoNav.css("display", "inline-block");
+        navbar.removeClass("navbar--top")
+        navbar.addClass("navbar--onscroll"); 
+
+       
+    } else if (window.scrollY < 400) {
+        buscar.addClass("navbar__buscar--top")
+        logoTop.css("display", "inline-block");
+        logoNav.css("display", "none");
+        navbar.removeClass("navbar--onscroll"); 
+        navbar.addClass("navbar--top")
+    }
+}
+
+$(document).scroll(navegadorCambio);
+
+$(document).ready(cargarPagina);
